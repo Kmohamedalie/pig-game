805dae5d1453d1db70e68c319cd0e7a229f6e61a
Mohamed Alie Kamara <63104472+Kmohamedalie@users.noreply.github.com>
2024-02-03 03:59:10 +0100

    project files

---
+++ script.js
@@ -0,0 +1,115 @@
+'use strict';
+// one is faster than the other when selecting 000's of elements
+
+//get sound effect from: https://pixabay.com/sound-effects/search/select%20sound/
+
+// Selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+// buttons
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
+
+// add audio
+// set a sound for roll and select
+const roll = document.createElement('audio');
+roll.src = 'roll.mp3';
+const select = document.createElement('audio');
+select.src = 'select.mp3';
+
+// declaring a variable outside
+let scores, currentScore, activePlayer, playing;
+
+const init = function () {
+  select.play();
+  // assigning a value inside the function
+  scores = [0, 0];
+  currentScore = 0;
+  activePlayer = 0;
+  playing = true;
+
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add('hidden');
+  player0El.classList.remove('player--winner');
+  player1El.classList.remove('player--winner');
+  player0El.classList.add('player--active');
+  player1El.classList.remove('player--active');
+};
+// initial the game by running the init function
+init();
+
+// switch player code/function
+const switchPlayer = function () {
+  // Switch to next player
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+// Rolling dice functionality
+btnRoll.addEventListener('click', function () {
+  // roll play sound
+  roll.play();
+  if (playing) {
+    //1. Generating a random dice roll
+    const dice = Math.trunc(Math.random() * 6) + 1;
+
+    // 2. Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    // 3. Check for rolled 1: if true, switch to next player
+    if (dice !== 1) {
+      // Add dice to current score
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      // switch to next player
+      switchPlayer();
+    }
+  }
+});
+
+// Hold score button and switch player
+btnHold.addEventListener('click', function () {
+  select.play();
+  if (playing) {
+    // 1. Add current score to active player's score
+    scores[activePlayer] += currentScore;
+    //scores[1] = scores[1] + currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    // 2. Check if player's score is >= 100
+    if (scores[activePlayer] >= 100) {
+      // Finish the game
+      playing = false;
+      diceEl.classList.add('hidden');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    }
+
+    // switch to next player
+    switchPlayer();
+  }
+});
+
+// New game
+btnNew.addEventListener('click', init);
