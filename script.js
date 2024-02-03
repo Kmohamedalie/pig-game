7262158f49dcae2343caa8a119a22180d71439e0
Mohamed Alie Kamara <63104472+Kmohamedalie@users.noreply.github.com>
2024-02-03 16:16:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -98,6 +98,7 @@ btnHold.addEventListener('click', function () {
     // 2. Check if player's score is >= 100
     if (scores[activePlayer] >= 100) {
       // Finish the game
+      roll.pause();
       success.play();
       playing = false;
       diceEl.classList.add('hidden');
